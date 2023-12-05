3a5edb9d766c7e197cceb2a3823c8b79dcbfb0c8
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2023-12-05 14:45:16 -0700

    Added a copy to clipboard button

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
 import { donationList } from "./donationList.mjs";
 import { reveal } from "./reveal.mjs";
 import { youTubeVideoHeight } from "./youTubeVideoHeight.mjs";
-import { openPopUp, closePopUp } from "./popup.js";
+import { openPopUp, closePopUp, copyToClip } from "./popup.js";
 import { readMore, readLess } from "./readMore.js"
-
 donationList();
 youTubeVideoHeight();
 if (window.innerWidth < 1024) {
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,17 +1,29 @@
+// For PopUp Buttons
 document.getElementById('shareButton').addEventListener('click', openPopUp);
 document.getElementById('popUpDoneBtn').addEventListener('click', closePopUp);
 
+// For Copy To Clipboard button
+document.getElementById('copyButton').addEventListener('click', copyToClip);
+
+// For PopUp Buttons
 var popUp = document.querySelector('.sharePopUp');
 var overlay = document.getElementById('overlay');
-
 export function openPopUp() {
     overlay.style.display = 'block';
     popUp.style.display = 'block';
     document.body.style.overflow = 'hidden'; // Prevent scrolling when the pop-up is open
 }
-
 export function closePopUp() {
     overlay.style.display = 'none';
     popUp.style.display = 'none';
     document.body.style.overflow = 'auto'; // Allow scrolling again
+}
+
+// For Copy To Clipboard button
+export function copyToClip() {
+    // Select the text in the textarea
+    var textArea = document.getElementById('textToCopy');
+    navigator.clipboard.writeText(textArea.innerText);
+    alert("Text copied!")
+    closePopUp();
 }
