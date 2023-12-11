76e23ca84a854a7a2a745146690fc4bf886066b5
michaelparkernorton <michaelparkernorton.va@gmail.com>
2023-12-11 17:27:29 -0300

    includeHTML function

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 import { donationList } from "./mjs/donationList.mjs";
 import { reveal } from "./mjs/reveal.mjs";
 import { youTubeVideoHeight } from "./mjs/youTubeVideoHeight.mjs";
+import { includeHTML } from "./mjs/includeHTML.mjs";
 import "./mjs/popup.mjs";
 import "./mjs/seeAll.mjs";
 import "./mjs/share.mjs";
-import { readMore, readLess } from "./mjs/readMore.mjs"
-
-import {  } from "module";
+import { readMore, readLess } from "./mjs/readMore.mjs";
+includeHTML();
 donationList();
 youTubeVideoHeight();
 if (window.innerWidth < 1024) {
