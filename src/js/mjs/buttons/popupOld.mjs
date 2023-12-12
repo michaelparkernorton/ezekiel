f807820c884f2a4a18025537d238263526c874b1
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2023-12-12 12:15:49 -0700

    more donation btn changes

--- src/js/mjs/buttons/popupOld.mjs
+++ src/js/mjs/buttons/popupOld.mjs
@@ -1,9 +1,10 @@
 document.getElementById('shareButton').addEventListener('click', openPopUp);
-document.querySelector('.donate-button').addEventListener('click', openDonatePopUp);
 document.getElementById('popUpDoneBtn').addEventListener('click', closePopUp);
+document.getElementById('donateButton').addEventListener('click', openDonatePopUp);
+document.getElementById('donatePopUpDoneBtn').addEventListener('click', closeDonatePopUp);
 
 let sharePopUp = document.querySelector('.sharePopUp');
-let donatePopUp = document.querySelector('.donate')
+let donatePopUp = document.querySelector('.donatePopUp')
 let overlay = document.getElementById('overlay');
 
 export function openPopUp() {
@@ -16,12 +17,16 @@ export function openDonatePopUp() {
     overlay.style.display = 'block';
     donatePopUp.style.display = 'block';
     document.body.style.overflow = 'hidden'; // Prevent scrolling when the pop-up is open
-    document.querySelector('.close-pop-up').addEventListener('click', closePopUp);
+}
+
+export function closeDonatePopUp(){
+    overlay.style.display = 'none';
+    donatePopUp.style.display = 'none';
+    document.body.style.overflow = 'auto'; // Allow scrolling again
 }
 
 export function closePopUp() {
     overlay.style.display = 'none';
     sharePopUp.style.display = 'none';
-    donatePopUp.style.display = 'none';
     document.body.style.overflow = 'auto'; // Allow scrolling again
 }
