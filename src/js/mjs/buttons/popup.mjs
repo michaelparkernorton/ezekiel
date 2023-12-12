67a476954ed4adca5f6dcdd59c69ec1c37a1687c
michaelparkernorton <michaelparkernorton.va@gmail.com>
2023-12-12 16:09:17 -0300

    css and popups

--- src/js/mjs/buttons/popup.mjs
+++ src/js/mjs/buttons/popup.mjs
@@ -1,11 +1,13 @@
-// For PopUp Buttons
-var overlay = document.getElementById('overlay');
+const overlay = document.getElementById('overlay');
+const popUp = document.querySelector('.popUp');
 export function openPopUp() {
     overlay.style.display = 'block';
+    popUp.style.display = 'flex';
     document.body.style.overflow = 'hidden'; // Prevent scrolling when the pop-up is open
 }
 export function closePopUp() {
     overlay.style.display = 'none';
+    popUp.style.display = 'none';
     document.body.style.overflow = 'auto'; // Allow scrolling again
 }
 
